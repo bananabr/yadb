d63b7778cc4e9f43bc88fa561ab28f4d21c09256
Daniel Santos <vovohelo@gmail.com>
2020-10-13 17:25:22 +0000

    made get-resolvers executable; send get-resolvers output to stdout

--- get-resolvers.sh
+++ get-resolvers.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 COUNTRY_CODE=${1:-"US"}
-
-curl https://public-dns.info/nameservers.csv | cut -d , -f 1,5,10,11 | grep $COUNTRY_CODE | grep ',0\.9.,' | sort --field-separator=',' -k 3 -r | cut -d , -f 1 | grep -v : > resolvers.txt
+curl https://public-dns.info/nameservers.csv 2> /dev/null | cut -d , -f 1,5,10,11 | grep $COUNTRY_CODE | grep ',0\.9.,' | sort --field-separator=',' -k 3 -r | cut -d , -f 1 | grep -v :
